29cee5979c3bdeddc296c07e8ab9515ea4961bc6
Atıl Samancıoğlu <samancioglu.atil@gmail.com>
2021-12-30 21:03:25 +0300

    Flows, StateFlow, LiveData added

---
+++ app/src/main/java/com/atilsamancioglu/flows/MainActivity.kt
@@ -0,0 +1,80 @@
+package com.atilsamancioglu.flows
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.atilsamancioglu.flows.ui.theme.FlowsTheme
+import kotlinx.coroutines.flow.collect
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            FlowsTheme {
+                val viewModel : MyViewModel by viewModels()
+                SecondScreen(viewModel = viewModel)
+            }
+        }
+    }
+}
+
+@Composable
+fun FirstScreen(viewModel: MyViewModel) {
+    val counter = viewModel.countDownTimerFlow.collectAsState(initial=10)
+
+    Surface(color = MaterialTheme.colors.background) {
+        Box(modifier = Modifier.fillMaxSize()) {
+            Text(text = counter.value.toString(),
+                fontSize = 26.sp,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.align(Alignment.Center)
+            )
+        }
+
+    }
+}
+
+@Composable
+fun SecondScreen(viewModel : MyViewModel) {
+
+    val liveDataValue = viewModel.liveData.observeAsState()
+    val stateFlowValue = viewModel.stateFlow.collectAsState()
+
+    Surface(color = MaterialTheme.colors.background) {
+      Box(modifier = Modifier.fillMaxSize()) {
+            Column(modifier = Modifier.align(Alignment.Center)) {
+                Text(text = liveDataValue.value ?: "")
+                Button(onClick = {
+                    viewModel.changeLiveDataValue()
+                }) {
+                    Text(text = "LiveData Button")
+                }
+                Spacer(modifier = Modifier.padding(10.dp))
+
+                Text(text = stateFlowValue.value ?: "")
+                Button(onClick = {
+                    viewModel.changeStateFlowValue()
+                }) {
+                    Text(text = "State Flow Button")
+                }
+
+            }
+      }
+    }
+}
---
+++ app/src/main/java/com/atilsamancioglu/flows/MyViewModel.kt
@@ -0,0 +1,81 @@
+package com.atilsamancioglu.flows
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+
+class MyViewModel : ViewModel() {
+
+    val countDownTimerFlow = flow<Int> {
+        val countDownFrom = 10
+        var counter = countDownFrom
+        emit(countDownFrom)
+        while(counter > 0) {
+            delay(1000)
+            counter--
+            emit(counter)
+        }
+    }
+
+    //Collect flow values within VM
+
+    init {
+        collectInViewModel()
+    }
+
+    private fun collectInViewModel() {
+        viewModelScope.launch {
+            countDownTimerFlow
+                .filter {
+                    it %3 == 0
+                }
+                .map {
+                    it + it
+                }
+                .collect {
+                println("counter is: ${it}")
+            }
+        /*
+            countDownTimerFlow.collectLatest {
+                delay(2000)
+                println("counter is: ${it}")
+            }
+
+         */
+
+        }
+
+        //Same with collect ->
+        /*
+        countDownTimerFlow.onEach {
+            println(it)
+        }.launchIn(viewModelScope)
+         */
+    }
+
+    //LiveData comparison
+
+    private val _liveData = MutableLiveData<String>("KotlinLiveData")
+    val liveData : LiveData<String> = _liveData
+
+    fun changeLiveDataValue() {
+        _liveData.value = "Live Data"
+    }
+
+    private val _stateFlow = MutableStateFlow("KotlinStateFlow")
+    val stateFlow = _stateFlow.asStateFlow()
+
+    private val _sharedFlow = MutableSharedFlow<String>()
+    val sharedFlow = _sharedFlow.asSharedFlow()
+
+
+    fun changeStateFlowValue() {
+        _stateFlow.value = "State Flow"
+    }
+
+
+}
